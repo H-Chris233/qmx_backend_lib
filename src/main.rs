0583bb1d8300cd40f45b96ddf1ee7569698f933d
H-Chris233 <3475310230@qq.com>
2025-06-09 16:27:51 +0800

    ...

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,41 @@
-use ui::agreement::show_agreement_window;
 
+
+use eframe::egui;
+
+use ui::agreement::show_agreement_window;
+use ui::window_manager::init_global_window;
+use ui::window_manager::GLOBAL_WINDOW;
+
+
+
+struct MyApplication {}
+
+impl Default for MyApplication {
+    fn default() -> Self {
+        Self {}
+    }
+}
+
+impl eframe::App for MyApplication {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        // 检查并显示全局窗口
+        if let Some(global_window) = GLOBAL_WINDOW.get().cloned() {
+            let mut guard = global_window.lock().unwrap();
+            if let Some(window) = guard.as_mut() {
+                window.show(ctx);
+            }
+        }
+    }
+}
 fn main() {
+    init_global_window();
     println!("hello");
     show_agreement_window();
+     // 启动 GUI 应用
+    let options = eframe::NativeOptions::default();
+    eframe::run_native(
+        "用户协议示例",
+        options,
+        Box::new(|_cc| Box::new(MyApplication::default())),
+    );
 }
