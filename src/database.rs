7fabfae7170545e9458bcf7eaf6037fd98162fad
H-Chris233 <h-chris233@outlook.com>
2025-07-01 16:47:35 +0800

    ...

--- src/database.rs
+++ src/database.rs
@@ -1,9 +1,8 @@
-
 use super::student::StudentDatabase;
 use super::cash::CashDatabase;
 
 use anyhow::{Context, Result};
-use log::{debug, info};
+use log::{debug, info, warn};
 use serde::{Deserialize, Serialize};
 
 /// 运行时数据库容器，不持久化到磁盘
@@ -38,12 +37,49 @@ impl Database {
 pub fn init() -> Result<Database> {
     info!("正在初始化运行时数据库");
 
-    // 分别加载两个子数据库
-    let student_db = StudentDatabase::read_from("./data/student_database.json")
-        .with_context(|| "加载学生数据库失败")?;
-    
-    let cash_db = CashDatabase::read_from("./data/cash_database.json")
-        .with_context(|| "加载现金数据库失败")?;
+    let student_db = match StudentDatabase::read_from("./data/student_database.json") {
+        Ok(db) => {
+            info!("学生数据库加载成功");
+            db
+        }
+        Err(e) => {
+            if let Some(io_err) = e.downcast_ref::<std::io::Error>() {
+                if io_err.kind() == std::io::ErrorKind::NotFound {
+                    warn!("学生数据库文件不存在，正在创建新的数据库...");
+                    let new_db = StudentDatabase::new();
+                    new_db.save()
+                        .with_context(|| "无法保存新建的学生数据库")?;
+                    new_db
+                } else {
+                    return Err(e).with_context(|| "加载学生数据库失败");
+                }
+            } else {
+                return Err(e).with_context(|| "加载学生数据库失败");
+            }
+        }
+    };
+
+    let cash_db = match CashDatabase::read_from("./data/cash_database.json") {
+        Ok(db) => {
+            info!("现金数据库加载成功");
+            db
+        }
+        Err(e) => {
+            if let Some(io_err) = e.downcast_ref::<std::io::Error>() {
+                if io_err.kind() == std::io::ErrorKind::NotFound {
+                    warn!("现金数据库文件不存在，正在创建新的数据库...");
+                    let new_db = CashDatabase::new();
+                    new_db.save()
+                        .with_context(|| "无法保存新建的现金数据库")?;
+                    new_db
+                } else {
+                    return Err(e).with_context(|| "加载现金数据库失败");
+                }
+            } else {
+                return Err(e).with_context(|| "加载现金数据库失败");
+            }
+        }
+    };
 
     info!("运行时数据库初始化完成");
     Ok(Database::new(student_db, cash_db))
